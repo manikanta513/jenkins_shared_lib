57e913763b4e2bd85a26430b1edbae74afbc5868
manikanta513 <manikanta513@gmail.com>
2024-02-04 23:50:57 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar'
+         sh '/opt/maven/bin/mvn clean package sonar:sonar'
     }
 }
